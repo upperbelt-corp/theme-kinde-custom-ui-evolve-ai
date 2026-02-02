6b246ec95a9930c79097d77cfaafc644c9cae67b
Olatunji Longe <5619338+Olatunji-Longe@users.noreply.github.com>
2026-02-02 16:43:42 -0500

    use client instead of react-dom/server-browser

--- kindeSrc/environment/pages/(kinde)/(login)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(login)/page.tsx
@@ -1,27 +1,51 @@
-"use server";
+// "use server";
+//
+// import { Widget } from "../../../../components/widget";
+// import { DefaultLayout } from "../../../../layouts/default";
+// import { type KindePageEvent } from "@kinde/infrastructure";
+// import React from "react";
+// import { renderToString } from "react-dom/server";
+// import { Root } from "../../../../root";
+//
+// const LoginPage: React.FC<KindePageEvent> = ({ context, request }) => {
+//   return (
+//     <Root context={context} request={request}>
+//       <DefaultLayout>
+//         <Widget
+//           heading={context.widget.content.heading}
+//           description={context.widget.content.description}
+//         />
+//       </DefaultLayout>
+//     </Root>
+//   );
+// };
+//
+// // Page Component
+// export default async function Page(event: KindePageEvent): Promise<string> {
+//   const page = await LoginPage(event);
+//   return renderToString(page);
+// }
 
+
+"use client"; // If using Next.js
+
+import React from "react";
 import { Widget } from "../../../../components/widget";
 import { DefaultLayout } from "../../../../layouts/default";
 import { type KindePageEvent } from "@kinde/infrastructure";
-import React from "react";
-import { renderToString } from "react-dom/server.browser";
 import { Root } from "../../../../root";
 
 const LoginPage: React.FC<KindePageEvent> = ({ context, request }) => {
-  return (
-    <Root context={context} request={request}>
-      <DefaultLayout>
-        <Widget
-          heading={context.widget.content.heading}
-          description={context.widget.content.description}
-        />
-      </DefaultLayout>
-    </Root>
-  );
+    return (
+        <Root context={context} request={request}>
+            <DefaultLayout>
+                <Widget
+                    heading={context.widget.content.heading}
+                    description={context.widget.content.description}
+                />
+            </DefaultLayout>
+        </Root>
+    );
 };
 
-// Page Component
-export default async function Page(event: KindePageEvent): Promise<string> {
-  const page = await LoginPage(event);
-  return renderToString(page);
-}
+export default LoginPage;
